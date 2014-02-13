491c6d3ec3392ca9747b3b3e8a683787444b10b8
mzmz <milton.zhumingzhen@gmail.com>
2014-02-13 18:32:00 +0800

    get message looking

--- struct_msg.rb
+++ struct_msg.rb
@@ -1,15 +1,16 @@
 #require './struct.rb'
 require 'pp'
 
+##split asn file to blocks
 items = []
-cnt = 0
+#cnt = 0
 File.open("36331-c00-asn", "r") do |f|
   item = []
   f.each do |line|
     if /::=/.match(line)
       items << item
       item = []
-      p cnt += 1
+      #p cnt += 1
     end
     item << line #until /^--/.match(line) #or line==nil
   end
@@ -17,11 +18,24 @@ end
 
 
 msgs = {}
-
 items.each do |item|
-  if /(.*-Message)\s*::=(.*)/.match(item[0])
-    msgs[$1] = item 
+  if /(.*)::=(.*)/.match(item[0])
+    msg_key = $1.strip
+    msgs[msg_key] = []
+    item[0] = $2
+    item.each do |line|
+      if a_os=line.split(',')
+        a_os.each do |a_pair|
+          if a_pair.split.length != 0
+            msgs[msg_key] << a_pair.split
+          end
+        end
+      end
+    end    
   end
 end
 
 pp msgs
+
+
+
